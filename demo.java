49102b27d7f86b0dbd8440e4e95e72ef174d23c8
unknown <sumangala.bhat18@gmail.com>
2024-07-30 21:30:38 +0530

    initail commit

---
+++ demo.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectApplication.class, args);
+                System.out.println("Hello world");
+		
+
+	}
+
+}
